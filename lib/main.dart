e69f1d393ad525ca003a3fce18050290e31fc0fe
Tyler Shelton <bullfrogdevstudio@gmail.com>
2019-12-15 21:45:49 -0500

    Module 26 completed

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey[300],
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://s.w-x.co/util/image/v/1574365015844_NEW_IMAGE_SHOWS_CENTER_OF_GALAXY_ENG.jpg?v=at&w=1280&h=720'),
+          ),
+        ),
       ),
     ),
   );
